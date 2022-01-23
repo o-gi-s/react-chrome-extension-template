ded13a757ff722cd3886abeaa171d893b6afe509
o-gi-s <you@example.com>
2022-01-24 00:12:07 +0900

    Initial commit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>App</div>;
+const App = () => <>
+  <div>Hoge</div>
+  <div>Hoge</div>
+</>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
