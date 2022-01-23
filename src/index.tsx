92fe2a1adb6fd0ea7865416b63f418e1132fcb50
o-gi-s <you@example.com>
2022-01-24 00:18:47 +0900

    Make component centered.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from '@material-ui/core';
+import { Box, Button } from '@material-ui/core';
 
 const App = () => <>
-  <Button color="primary">Hello World</Button>
+  <Box width="324px" textAlign="center"> 
+    <Button color="primary">Hello World</Button>
+  </Box>
 </>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
